a24d382bfd5fefba2e380ddffafb812351c89d27
Lunfir <lunfirok@gmail.com>
2021-10-31 12:37:32 +0200

    enums

---
+++ c/enum.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+typedef enum
+{
+    Ready = 1,
+    Busy  = 2,
+    Completed = 4
+} State;
+
+
+int main()
+{
+    State state;
+
+    scanf("%d", &state);
+
+    switch(state)
+    {
+        case Ready:
+        printf("State Ready\n");
+        break;
+
+        case Busy:
+        printf("State Busy\n");
+        break;
+
+        case Completed:
+        printf("State Completed\n");
+        break;
+
+        default:
+        printf("Unknown State\n");
+        break;
+    }
+
+
+    int currentState = Ready | Completed;
+
+    if (state & currentState)
+    {
+        printf("currentState\n");
+    }
+
+
+
+    return 0;
+}
